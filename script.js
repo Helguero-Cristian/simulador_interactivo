75d8b4bddbb7b2821525b5a372577882fb95ccb2
Helguero Cristian <helguero.cristian@gmail.com>
2022-04-21 00:16:26 -0300

    aplicando estilos basicos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const IVA = 1.21;
 let nombreJuego = prompt("Ingresa el nombre del videojuego").toUpperCase();
-let precioJuego = parseFloat(prompt("Ingresa el precio del producto"));
+let precioJuego = parseFloat(prompt("Ingresa el precio del videojuego"));
 
+//Creo una funcion para calcular el IVA
 function calcularIVA (precio) {
     let impuesto = (precio * IVA)
     return impuesto
@@ -10,19 +11,19 @@ function calcularIVA (precio) {
 let resultado = calcularIVA (precioJuego)
 alert ("El precio final de " + nombreJuego + " es " + resultado)
 
-
-for (let index = 0; index < 5; index++) {
-    let entrada = prompt ("quiere realizar otra operacion")
+//Armo un ciclo para repetetir la operacion hasta que se cumpla la condicion
+for (let index = 0; index <= 5; index++) {
+    let entrada = prompt ("¿Quiere realizar otra operacion?\nY(si) / N(no)\n\nAtencion: Solo se permiten 5 consultas diarias").toUpperCase();
     if (entrada == "Y") {
         nombreJuego = prompt("Ingresa el nombre del videojuego").toUpperCase();
-        precioJuego = parseFloat(prompt("Ingresa el precio del producto"));
+        precioJuego = parseFloat(prompt("Ingresa el precio del juego"));
         let resultado = calcularIVA (precioJuego)
         alert ("El precio final de " + nombreJuego + " es " + resultado)
     } else if (entrada == "N") {
-        alert ("gracias")
+        alert ("Gracias por usar nuestro sistema")
         break
     } else {
-        alert ("ingrese una opcion valida")
+        alert ("Ingrese una opcion valida")
     }
 }
 
